9993315cd79aa81731e131171c297fff68461922
eduard.levchenko@arcelormittal.com <eduard.levchenko@arcelormittal.com>
2019-06-09 23:31:16 +0300

    add side list project

---
+++ WebUI/Areas/Project/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Areas.Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        // GET: Project/Home
+        public ActionResult Index()
+        {
+            return View();
+        }        
+        // GET: Project/Home
+        public ActionResult Index1()
+        {
+            return View();
+        }
+        public ActionResult ProjectList()
+        {
+            return View();
+        }  
+        public ActionResult ProgrammList()
+        {
+            return View();
+        }          
+
+    }
+}
