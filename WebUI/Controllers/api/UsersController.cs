390772c90c876a8a9bfd96339c5af2c800259e1f
Levchenko <eduard.levchenko@arcelormittal.com>
2019-07-30 13:32:32 +0300

    add metod GetUsersOfName

--- WebUI/Controllers/api/UsersController.cs
+++ WebUI/Controllers/api/UsersController.cs
@@ -62,5 +62,55 @@ namespace WebUI.Controllers.api
                 return NotFound();
             }
         }
+
+        // GET: api/users/user_name/ealevchenko
+        [Route("user_name/{user_name}")]
+        [ResponseType(typeof(Users))]
+        public IHttpActionResult GetUsersOfName(string user_name)
+        {
+            try
+            {
+                if (String.IsNullOrWhiteSpace(user_name)) return NotFound();
+                
+                
+                Users user = this.ef_us.Get()
+                    .Where(u => u.user_name == @"EUROPE\"+ user_name)
+                    .ToList()
+                    .Select(u => new Users
+                    {
+                        id = u.id,
+                        user_name = u.user_name,
+                        description = u.description,
+                        email = u.email,
+                        surname = u.surname,
+                        name = u.name,
+                        patronymic = u.patronymic,
+                        id_structural_subdivisions = u.id_structural_subdivisions,
+                        StructuralSubdivisions = new StructuralSubdivisions
+                        {
+                            id = u.StructuralSubdivisions.id,
+                            position = u.StructuralSubdivisions.position,
+                            name_subdivisions_ru = u.StructuralSubdivisions.name_subdivisions_ru,
+                            name_subdivisions_en = u.StructuralSubdivisions.name_subdivisions_en,
+                            name_subdivisions_full_ru = u.StructuralSubdivisions.name_subdivisions_full_ru,
+                            name_subdivisions_full_en = u.StructuralSubdivisions.name_subdivisions_full_en,
+                            type = u.StructuralSubdivisions.type,
+                            code = u.StructuralSubdivisions.code,
+                            parent_id = u.StructuralSubdivisions.parent_id,
+                        }
+                    }).FirstOrDefault();
+                if (user == null)
+                {
+                    return NotFound();
+                }
+                return Ok(user);
+            }
+            catch (Exception e)
+            {
+                return NotFound();
+            }
+        }
+
+
     }
 }
